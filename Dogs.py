75aaa66c0f4c44b6d275006cbce96314e4143035
Elen2056 <eltacombis@gmail.com>
2024-11-04 17:18:09 +0300

    Добавлен прогресс-бар

--- Dogs.py
+++ Dogs.py
@@ -2,6 +2,8 @@ import requests
 from tkinter import *
 from PIL import Image, ImageTk
 from io import BytesIO
+from tkinter import ttk
+
 
 def get_random_dog_image():
     try:
@@ -28,19 +30,32 @@ def show_image():
             label.image = img
 
 
-        except requests.RequestException as e:
+        except Exception as e:
             messagebox.showerror("Ошибка", f"Не удалось загрузить изображение: {e}")
+    # Останавливаем прогрессбар после загрузки картинки
+    progress.stop()
+
+def prog():
+    # Ставим прогрессбар в начальное положение
+    progress['value'] = 0
+    # Запускаем прогрессбар и увеличиваем значение от 0 до 100 за 3 секунды
+    progress.start(30)
+    window.after(3000, show_image)
 
 
 window = Tk()
-window.title("Картинки с собачками")
-window.geometry("360x420")
+window.title("Случайное изображение песика")
+# window.geometry("360x420")
 
-label = Label()
+label = ttk.Label()
 label.pack(padx=10, pady=10)
 
-button = Button(text="Загрузить изображение", command=show_image)
+button = ttk.Button(text="Загрузить изображение", command=prog)
 button.pack(padx=10, pady=10)
+
+# Используем ttk.Progressbar для индикации загрузки
+progress = ttk.Progressbar(mode='determinate', length=300)
+progress.pack(padx=10, pady=10)
 
 window.mainloop()
 
